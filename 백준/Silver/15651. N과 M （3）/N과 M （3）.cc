06de999b71d9287d95ac2c3f8e719eab85613fd8
kokodakadokok <45879491+kokodak@users.noreply.github.com>
2023-09-25 20:29:55 +0900

    [Silver III] Title: N과 M (3), Time: 384 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+using namespace std;
+
+const int INF = 987654321;
+const int MAX = 32767;
+const int MOD = 1000000000;
+
+/*
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+*/
+
+bool vi[9] = { false, };
+int arr[9];
+
+void backtrack(int cnt, int n, int m) {
+	if (cnt == m) {
+		/*
+		int cntt = 0;
+		for (int i = 0; i < m - 1; i++) {
+			if (arr[i] > arr[i + 1]) break;
+			else cntt++;
+		}
+		*/
+
+		//if (cntt == m - 1) {
+			for (int i = 0; i < m; i++) cout << arr[i] << " ";
+			cout << "\n";
+		//}
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (1) {
+			vi[i] = true;
+			arr[cnt] = i;
+			backtrack(cnt + 1, n, m);
+			vi[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	cin >> n >> m; //1 ~ n까지 m개인 수열 다 출력
+
+	backtrack(0, n, m);
+	return 0;
+}
