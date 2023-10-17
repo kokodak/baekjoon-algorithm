3dccf2f07589b9c09eb9b7ce341182f1103d1560
kokodakadokok <45879491+kokodak@users.noreply.github.com>
2023-10-18 01:29:05 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std;
+const int INF = 2e9;
+const int inf = 1e9;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cout << "I'm Sexy";
+	return 0;
+}
