0a15065959dae6967fcec3a55339e7d314ab2e7c
kokodakadokok <45879491+kokodak@users.noreply.github.com>
2023-11-13 17:38:43 +0900

    [Gold IV] Title: 친구비, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.cc
+++ 백준/Gold/16562. 친구비/친구비.cc
@@ -1,53 +1,55 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
+#define X first
+#define Y second
 using namespace std;
-const int INF = 2e9;
-const int inf = 1e9;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const ll INF = 1e18;
+const int inf = 2e9;
+const int SIZE = 1 << 18;
+const int MOD = 1e9 + 7;
 
-int arr[10001];
-int p[10001];
-bool visited[10001];
+int p[10010];
+int arr[10010];
 
 int find(int x) {
 	if (p[x] < 0) return x;
 	return p[x] = find(p[x]);
 }
 
-void merge(int a, int b) {
-	a = find(a);
-	b = find(b);
-	if (a == b) return;
-	if (arr[a] < arr[b]) p[b] = a;
-	else p[a] = b;
+void merge(int u, int v) {
+	u = find(u);
+	v = find(v);
+	if (u == v) return;
+	if (arr[u] < arr[v]) {
+		p[u] += p[v];
+		p[v] = u;
+	}
+	else {
+		p[v] += p[u];
+		p[u] = v;
+	}
 }
 
 int main(void){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
+	memset(p, -1, sizeof(p));
 	int n, m, k;
 	cin >> n >> m >> k;
-	for (int i = 1; i <= n; i++) {
-		cin >> arr[i];
-	}
-	memset(p, -1, sizeof(p));
+	for (int i = 1; i <= n; i++) cin >> arr[i];
 	for (int i = 0; i < m; i++) {
-		int v, w;
-		cin >> v >> w;
-		merge(v, w);
+		int u, v;
+		cin >> u >> v;
+		merge(u, v);
 	}
-	int ans = 0;
+	int summ = 0;
 	for (int i = 1; i <= n; i++) {
-		int root = find(i);
-		if (root == -1) {
-			visited[root] = true;
-			ans += arr[root];
-			continue;
-		}
-		if (visited[root]) continue;
-		visited[root] = true;
-		ans += arr[root];
+		if (p[i] < 0) summ += arr[i];
 	}
-	if (ans > k) cout << "Oh no";
-	else cout << ans;
+	if (summ > k) cout << "Oh no";
+	else cout << summ;
 	return 0;
 }
