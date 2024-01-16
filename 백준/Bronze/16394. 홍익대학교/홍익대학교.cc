c0de1ade4e1acd4e5e46d70a6d93054fc7c3a02a
Seungyong Lee <45879491+kokodak@users.noreply.github.com>
2024-01-16 19:06:24 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+const int INF = 2e9;
+const int inf = 1e9;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+	cout << n - 1946;
+	return 0;
+}
